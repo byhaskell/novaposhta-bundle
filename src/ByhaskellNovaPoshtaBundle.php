d1a9065c1fffbf77a4e19b277845340aa7f43033
haskell <by.haskell@gmail.com>
2025-09-13 12:44:23 +0300

    fix naming bundle

--- src/ByhaskellNovaPoshtaBundle.php
+++ src/ByhaskellNovaPoshtaBundle.php
@@ -3,4 +3,4 @@ namespace byhaskell\NovaPoshtaBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class NovaPoshtaBundle extends Bundle {}
+class ByhaskellNovaPoshtaBundle extends Bundle {}
--- src/DependencyInjection/ByhaskellNovaPoshtaExtension.php
+++ src/DependencyInjection/ByhaskellNovaPoshtaExtension.php
@@ -6,15 +6,15 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class NovaPoshtaExtension extends Extension
+class ByhaskellNovaPoshtaExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('novaposhta.api_key', $config['api_key']);
-        $container->setParameter('novaposhta.base_url', $config['base_url'] ?? 'https://api.novaposhta.ua/v2.0/json/');
+        $container->setParameter('byhaskell_novaposhta.api_key', $config['api_key']);
+        $container->setParameter('byhaskell_novaposhta.base_url', $config['base_url'] ?? 'https://api.novaposhta.ua/v2.0/json/');
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         if (file_exists(__DIR__.'/../../config/services.yaml')) {
